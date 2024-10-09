fdd4cf3ed2327ee819b9070ce477c923629e7739
ikennaeze <ikemanofkings@outlook.com>
2024-10-09 23:44:32 +0100

    react goes crazy

---
+++ React_Hooks_Checkpoint/src/Movies.js
@@ -0,0 +1,31 @@
+const movies = [
+    {
+        title: 'Coming from Insanity',
+        description: 'Best Nollywood movie ever',
+        posterURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREAEZcowUuJzatiXVz84b8CxhsXYOFoDVVKA&s',
+        rating: '4.5',
+    },
+    
+    {
+        title: 'Spider-Man: Across the Spider-Verse',
+        description: 'Best movie in the bane of existence',
+        posterURL: 'https://cdns-images.dzcdn.net/images/cover/e18d332ae96159ea6f8143dddb64ef9f/500x500.jpg',
+        rating: '5',
+    },
+
+    {
+        title: 'The Amazing World Of Gumball',
+        description: 'Funniest Cartoon ever',
+        posterURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyV_sVoDuKmQa3_h03BIBOx_lvcFiyu-6I8w&s',
+        rating: '5',
+    },
+
+    {
+        title: 'Ripples of love',
+        description: 'Garbage tier nollywood romance slop',
+        posterURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJypMKsNDhnGn3jAIUdGCuOC2zFEY4WS_TvA&s',
+        rating: '0',
+    },
+]
+
+export default movies
