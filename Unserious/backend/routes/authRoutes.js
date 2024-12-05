3b135be1c4ef058379b140d72d39f437b71326bb
ikennaeze <ikemanofkings@outlook.com>
2024-12-05 21:43:52 +0100

    N E X T

--- Unserious/backend/routes/authRoutes.js
+++ Unserious/backend/routes/authRoutes.js
@@ -3,7 +3,12 @@ const router = express.Router()
 const cors = require('cors')
 
 //allowing cors origin access
-
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 //authController api routes
 const {registerUser, loginUser, getUser} = require('../controllers/authController')
