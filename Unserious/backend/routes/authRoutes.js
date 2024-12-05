ffeb7b905048441b2d9fd42966574af060fa1066
ikennaeze <ikemanofkings@outlook.com>
2024-12-05 20:39:52 +0100

    FUCKING WORK PLS

--- Unserious/backend/routes/authRoutes.js
+++ Unserious/backend/routes/authRoutes.js
@@ -3,12 +3,13 @@ const router = express.Router()
 const cors = require('cors')
 
 //allowing cors origin access
-router.use(
-    cors({
-        credentials: true,
-        origin: ["http://localhost:5173", "https://unseriousfrontend.vercel.app"]
-    })
-)
+router.use(cors())
+// router.use(
+//     cors({
+//         credentials: true,
+//         origin: ["http://localhost:5173", "https://unseriousfrontend.vercel.app"]
+//     })
+// )
 
 //authController api routes
 const {registerUser, loginUser, getUser} = require('../controllers/authController')
