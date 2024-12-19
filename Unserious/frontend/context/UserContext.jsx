9934590ebad4efef268405fccf6dea0c959c1b1a
ikennaeze <ikemanofkings@outlook.com>
2024-12-19 15:08:31 +0100

    usered

--- Unserious/frontend/context/UserContext.jsx
+++ Unserious/frontend/context/UserContext.jsx
@@ -22,7 +22,7 @@ export function UserContextProvider({children}) {
                 })
             }
         }
-    }, [])
+    }, [user])
     
     return (
         <UserContext.Provider value={{user, setUser}}>
